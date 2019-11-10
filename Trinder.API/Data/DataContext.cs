fc571232dec855577b3ce1ead8b946b59f789798
adammarc93 <adammarc93@gmail.com>
2019-11-10 16:43:46 +0100

    Created first model Value

---
+++ Trinder.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Trinder.API.Models;
+
+namespace Trinder.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
